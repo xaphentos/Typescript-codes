bbb5664692cc1d3e926f2ff05570aca1d2bb6861
Aaron <aaronfaminski@outlook.com>
2025-05-15 21:27:28 +0300

    Create variables.ts

---
+++ variables.ts
@@ -0,0 +1,21 @@
+// Variables
+let variable1;
+console.log(variable1); // undefined
+
+// 2. Declaring typed variables
+let num: number = 5;
+let str: string = "hello";
+let bool: boolean = true;
+
+console.log(num);   // 5
+console.log(str);   // hello
+console.log(bool);  // true
+
+// 3. Changing variable values
+num = 10;
+str = "world";
+bool = false;
+
+console.log(num);   // 10
+console.log(str);   // world
+console.log(bool);  // false
